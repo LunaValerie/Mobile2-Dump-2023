46b8da2766026af06121b3c8f19c6320d5a41eed
LunaUI <59039458+LunaValerie@users.noreply.github.com>
2023-08-29 13:39:23 -0800

    ce
    ce

---
+++ dump.cpp
@@ -0,0 +1,25 @@
+Cheat Engine Dump
+@LunaValerie
+|{LocalPlayer}|
+LocalPlayer > "metin2f-Win64-Shipping.exe"+03C03DA0 - DWORD
+Local Pointer > 0x2F8 - DWORD 
+Attack Speed > 0x8A8 - DWORD
+Movement Speed > 0x86C - Float
+Megic Speed > 0x8AC - DWORD
+SP > 0x8B0 - DWORD
+HP > 0x85C
+XP > 0x898
+Magic Defense > 0x8B4 - DWORD
+Int > 0x8B8 - DWORD
+Vıt > 0x8BC - DWORD
+Dex > 0x8C0 - DWORD
+Str > 0x8C4 - DWORD
+Attack Value > 0x8D0 - DWORD
+Defense > 0x8D4 - DWORD
+
+|{Others - Asm}|
+Server is full > metin2f-Win64-Shipping.exe+8DE277
+
+|{Username Password}
+Namespace : \x45\x7A\x67\x69\x7C\x59\x65\x6C\x6C\x6F\x77\x65\x73\x31\x32\x33\x7C\x61\x38\x35\x65\x34\x35\x65\x36\x62\x37\x33\x36\x7C\x37\x05
+Mask : xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
